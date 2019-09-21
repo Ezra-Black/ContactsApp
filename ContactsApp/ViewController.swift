e2d361731264ed9926f9083516cef4f2c20d3810
JosephRogers94 <Joerogersss2016@gmail.com>
2019-09-21 14:15:32 -0500

    Base Project

---
+++ ContactsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Joseph Rogers on 9/21/19.
+//  Copyright © 2019 Joe Rogers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
